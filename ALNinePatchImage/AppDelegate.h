4e701542c96d4d04708c6c6d30479d8853e201b4
Alex Lee <liyiqiang@maxthon.net>
2014-09-19 00:51:12 +0800

    Initial Commit

---
+++ ALNinePatchImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALNinePatchImage
+//
+//  Created by Alex Lee on 9/19/14.
+//  Copyright (c) 2014 Alex Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALNinePatchImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALNinePatchImage
+//
+//  Created by Alex Lee on 9/19/14.
+//  Copyright (c) 2014 Alex Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
